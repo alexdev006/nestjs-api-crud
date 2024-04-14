df7be447aa5a9b28da163cbe57feb24d03599041
Alex <alexdev0605@gmail.com>
2024-04-14 19:19:56 +0200

    feat: first route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return { msg: 'im signup' };
+  }
+
+  signin() {
+    return { msg: 'im singnin' };
+  }
+}
